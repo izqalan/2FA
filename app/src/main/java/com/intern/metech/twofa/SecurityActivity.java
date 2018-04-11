1f76faba39da2fa14f4578d6475c14c3e7b86f5e
izqalan <izqalan@gmial.com>
2018-04-11 17:59:29 +0800

    Added QR code for scan

--- app/src/main/java/com/intern/metech/twofa/SecurityActivity.java
+++ app/src/main/java/com/intern/metech/twofa/SecurityActivity.java
@@ -1,10 +1,17 @@
 package com.intern.metech.twofa;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,6 +19,11 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SecurityActivity extends AppCompatActivity {
 
@@ -25,6 +37,7 @@ public class SecurityActivity extends AppCompatActivity {
         Intent intent = getIntent();
         final String username = intent.getStringExtra("username");
         final String authToken = intent.getStringExtra("authToken"); // TOKEN FOR COMPARISON
+        secret.setText(authToken);
 
         // CRASHED  HERE !!
 
@@ -37,24 +50,64 @@ public class SecurityActivity extends AppCompatActivity {
                     try {
                         JSONObject jsonResponse = new JSONObject(response);
                         boolean success = jsonResponse.getBoolean("success");
+                        String key = jsonResponse.getString("authToken");
+                        String qrUrl = jsonResponse.getString("qrCodeUrl");
                         if(success)
                         {
                             TextView secret = findViewById(R.id.secret);
-                            Intent intent = getIntent();
-                            String key = intent.getStringExtra("authToken");
                             secret.setText(key);
+                            new DownloadImageFromInternet((ImageView) findViewById(R.id.qr))
+                                    .execute(qrUrl);
                         }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }else
+                {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        String qrUrl = jsonResponse.getString("qrCodeUrl");
+                        new DownloadImageFromInternet((ImageView) findViewById(R.id.qr))
+                                .execute(qrUrl);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
             }
         };
-        String key = intent.getStringExtra("authToken");
-        secret.setText(key);
+
         SecretRequest secretRequest = new SecretRequest(username, responseListener);
         RequestQueue q = Volley.newRequestQueue(SecurityActivity.this);
         q.add(secretRequest);
 
     }
+
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView = imageView;
+            Toast.makeText(getApplicationContext(), "Please wait, it may take a few minute...", Toast.LENGTH_SHORT).show();
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
+
 }
